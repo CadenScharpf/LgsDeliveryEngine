957aed7c2aadace6dcf66f77db16622aaedd5270
Joseph Harrington <jharrin4@asu.edu>
2022-07-19 23:27:41 -0700

    attempting to recreate database demo

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import { createDrawerNavigator } from '@react-navigation/drawer';
 import Dashboard from './components/dashboard/Dashboard'; 
 import Scanner from './components/scanner/Scanner'
 import Settings from './components/settings/Settings'
+import Demo_database from './components/demo_database/Demo_database'
 
 
 const Drawer = createDrawerNavigator();
@@ -16,6 +17,7 @@ function SideBar() {
       <Drawer.Screen name="Dashboard" component={Dashboard} />
       <Drawer.Screen name="Scan Barcode" component={Scanner} />
       <Drawer.Screen name="Settings" component={Settings} />
+      <Drawer.Screen name="Demo - Database" component={Demo_database} />
     </Drawer.Navigator>
   );
 }
